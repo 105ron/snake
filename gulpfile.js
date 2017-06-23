6527807a7e8234cb85d3376b0eafbbd7c31f2998
Rhys <rbrooker105@gmail.com>
2017-06-23 21:04:49 +1000

    add basic file structure, install gulp to prefeix css

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+  gulp.src('build/styles.css')
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('stylesheets'))
+  .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+    browser: ["google chrome", "firefox"]
+  })
+})
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('build/styles.css', ['styles']);
+  gulp.watch('index.html', browserSync.reload);
+})

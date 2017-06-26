50a7cc6b4216329207a4e3c23b13ed57f5aefba0
Rhys <rbrooker105@gmail.com>
2017-06-26 18:10:44 +1000

    add snake class and draw snake on grid

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,11 +1,32 @@
-
+'use strict';
 const gameContainer = document.getElementById('game-container');
-const gameGrid = Array.apply(null, Array(42)).map( (y, yIndex) => { 
-  let row = Array.apply(null, Array(56)).map( (x, xIndex) => {
-    return `<div class="game-squares ${ yIndex }-${ xIndex }"></div>`;
-  });
-  row = `<div class ="row">${ row.join('') }</div>`
-  return row;
-});
+let row;
+let snake = {
+  position: [28,21],
+  direction: "right",
+  current: [[26, 21], [27,21], [28,21], [29,21]],
+};
 
-gameContainer.insertAdjacentHTML('afterbegin', gameGrid.join(''));
+const coordinatesToClass = function coordinatesToClass(position) {
+  let gridCoordinates = `${ position[0] }-${ position[1] }`;
+  return document.getElementsByClassName(gridCoordinates);
+};
+
+
+let drawSnake = function drawSnake() {
+  snake.current.forEach( (cordinates) => {
+    let snakeDiv = coordinatesToClass(cordinates);
+    snakeDiv[0].classList.toggle('snake');
+  });
+};
+
+(function render() {
+  const gameGrid = Array.apply(null, Array(43)).map( (y, yIndex) => { 
+    row = Array.apply(null, Array(56)).map( (x, xIndex) => {
+      return `<div class="game-squares ${ xIndex }-${ yIndex }"></div>`;
+    });
+    return `<div class ="row">${ row.join('') }</div>`;
+  });
+  gameContainer.insertAdjacentHTML('afterbegin', gameGrid.join(''));
+  drawSnake();
+})();

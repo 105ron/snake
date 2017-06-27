ce28b64bb537c929617e2986459febe52ccba72e
Rhys <rbrooker105@gmail.com>
2017-06-27 21:23:20 +1000

    add function to move co-ordinates of snake

--- scripts/snake.js
+++ scripts/snake.js
@@ -4,20 +4,49 @@ let row;
 let snake = {
   position: [28,21],
   direction: "right",
+  //this is the co-ordinates, goes tail --> head
   current: [[26, 21], [27,21], [28,21], [29,21]],
 };
 
 const coordinatesToClass = function coordinatesToClass(position) {
   let gridCoordinates = `${ position[0] }-${ position[1] }`;
+  //return the div element with the class
   return document.getElementsByClassName(gridCoordinates);
 };
 
-
-let drawSnake = function drawSnake() {
+const drawSnake = function drawSnake() {
   snake.current.forEach( (cordinates) => {
     let snakeDiv = coordinatesToClass(cordinates);
+    //each grid co-ordinate is unique. snakeDiv.length == 1 so we want to change the class on element[0]
     snakeDiv[0].classList.toggle('snake');
   });
+};
+
+Array.prototype.SumArray = function (arr) {
+    let sum = this.map(function (num, index) {
+      return num + arr[index];
+    });
+    return sum;
+  }
+
+const moveHead = function moveHead(coordinates, shift) {
+  const direction = {
+    right: [1, 0],
+    left: [-1, 0],
+    down: [0, 1],
+    up: [0,-1],
+  };
+  return coordinates.SumArray(direction[shift]);
+};
+
+const moveSnake = function moveSnake() {
+  let headCordinates = snake.current[snake.current.length -1];
+  headCordinates = moveHead(headCordinates, snake.direction);
+  let current = snake.current;
+  let newCordinates = [...current, [...headCordinates]];
+  newCordinates.splice(0,1);
+  snake.current = newCordinates;
+  return newCordinates;
 };
 
 (function render() {

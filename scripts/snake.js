34f68fcc27647100a0b188126bfa3cb00241282f
Rhys <rbrooker105@gmail.com>
2017-07-01 21:36:45 +1000

    add logic for snake moving off board or hitting it's body

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 let row;
 let fps = 10;
 let now;
@@ -9,49 +9,18 @@ let snake = {
   position: [28,21],
   direction: "right",
   //this is the co-ordinates, goes tail --> head
-  current: [[26, 21], [27,21], [28,21], [29,21]],
+  current: [[20, 21], [21,21], [22,21],[23, 21], [24,21], [25,21], [26, 21], [27,21], [28,21], [29,21]],
 };
 const gameContainer = document.getElementById('game-container');
 
-document.onkeydown = function(evt) {
-  const keys = {
-    37: 'left', //arrow keys
-    38: 'up',
-    39: 'right',
-    40: 'down',
-    65: 'left', //wasd keys
-    87: 'up',
-    68: 'right',
-    83: 'down'
-  };
-  const illegalMoves = {
-    left: 'right',
-    right: 'left',
-    up: 'down',
-    down: 'up',
-  }
-  evt = evt || window.event;
-  console.log(evt);
-  if (keys[evt.keyCode]) {
-    if ((illegalMoves[snake.direction]) != (keys[evt.keyCode])) {
-    console.log(keys[evt.keyCode] + "event");
-    console.log(illegalMoves[snake.direction]);
-    console.log(Boolean((illegalMoves[snake.direction]) != (keys[evt.keyCode])));
-      snake.direction = keys[evt.keyCode];
-  } else {
-    alert ('don\'t do that');
-  }
-  }
-};
-
-const coordinatesToDiv = function coordinatesToDiv(position) {
+const getDiv = function getDiv(position) {
   let gridCoordinates = `${ position[0] }-${ position[1] }`;
   //return the div element with the class
   return document.getElementsByClassName(gridCoordinates);
 };
 
 const toggleSnakeClass = function toggleSnakeClass(coordinates) {
-  let changeClassDiv = coordinatesToDiv(coordinates);
+  let changeClassDiv = getDiv(coordinates);
   //each grid co-ordinate is unique. snakeDiv.length == 1 so we want to change the class on element[0]
   changeClassDiv[0].classList.toggle('snake');
 };
@@ -90,22 +59,69 @@ const moveHead = function moveHead(coordinates, shift) {
   gameContainer.insertAdjacentHTML('afterbegin', gameGrid.join(''));
   drawSnake();
 })();
+
+document.onkeydown = function(evt) {
+  const keys = {
+    37: 'left', //arrow keys
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'left', //wasd keys
+    87: 'up',
+    68: 'right',
+    83: 'down'
+  };
+  const illegalMoves = {
+    left: 'right',
+    right: 'left',
+    up: 'down',
+    down: 'up',
+  }
+  evt = evt || window.event;
+  if (keys[evt.keyCode]) {
+    if ((illegalMoves[snake.direction]) != (keys[evt.keyCode])) {
+      snake.direction = keys[evt.keyCode];
+    }
+  }
+};
+
+const moveSnake = function moveSnake(current, movedCordinates) {
+  let newCordinates = [...current, [...movedCordinates]];
+  newCordinates.splice(0,1);
+  toggleSnakeClass(movedCordinates); //draw the head
+  toggleSnakeClass(current[0]); //erase the tail
+  return newCordinates;
+};
+
+const isSameCoordinates = function isSameCoordinates(position, snakeArray) {
+  let body = [...snakeArray]
+  body.pop(); //remove head before we search for it's coordinates in it's positions
+  return body.some( xAndY => 
+    xAndY.every( (x, i) => x === position[i])
+  );
+};
+
+
+const isValidPosition = function isValidPosition(position, snakeArray) {
+  if (getDiv(position).length < 1) { return false }; //snake is off the board
+  if (isSameCoordinates(position, snakeArray)) {
+    console.log("clashing");
+  }
+  return true;
+};
   
 function move() { 
     requestAnimationFrame(move);
     now = Date.now();
     delta = now - then;
     if (delta > interval) {
-        then = now - (delta % interval);
-        // ... Code for Drawing the Frame ...
-        let headCordinates = snake.current[snake.current.length -1];
-        headCordinates = moveHead(headCordinates, snake.direction);
-        let current = snake.current;
-        let newCordinates = [...current, [...headCordinates]];
-        newCordinates.splice(0,1);
-        toggleSnakeClass(headCordinates); //draw the head
-        toggleSnakeClass(current[0]); //erase the tail
-        snake.current = newCordinates;
+      then = now - (delta % interval);
+      let headCoordinates = snake.current[snake.current.length -1];
+      headCoordinates = moveHead(headCoordinates, snake.direction);
+      if (isValidPosition(headCoordinates, snake.current)) {
+        // console.log(`${ headCoordinates } off the board`);
+      };
+      snake.current = moveSnake(snake.current, headCoordinates);
     }
 }
  
